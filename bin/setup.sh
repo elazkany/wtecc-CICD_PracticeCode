5af2ba7e32e38df2a99186f0b87074271d040f1a
elazkany <142848290+elazkany@users.noreply.github.com>
2025-06-12 22:03:46 +0000

    Installed kubectl

---
+++ bin/setup.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install gh
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+chmod +x kubectl
+mkdir -p ~/.local/bin
+mv ./kubectl ~/.local/bin/kubectl
+rm -f kubectl.sha256
